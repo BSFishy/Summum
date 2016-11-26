58ab40b897143d0423a5b20bc428a12fa970d636
Lousy Lynx <mttprvstanddad@gmail.com>
2016-11-25 19:21:38 -0600

    Bump to v0.3.1

--- src/main/java/com/lousylynx/summum/SummumMod.java
+++ src/main/java/com/lousylynx/summum/SummumMod.java
@@ -22,7 +22,7 @@ public final class SummumMod {
 
     public static final String MODID = "summum";
     public static final String NAME = "Summum";
-    public static final String VERSION = "0.3";
+    public static final String VERSION = "0.3.1";
     public static final String DEPENDENCIES = "required-after:Forge@[12.18.2.2116,);";
     /*public static final String GUI_FACTORY = "";*/
 
